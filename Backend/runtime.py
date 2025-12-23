379d3a93a5071b262dd295865f3bf725b21ab475
moob453 <85918186+moob453@users.noreply.github.com>
2025-12-23 21:10:51 +1030

    Enhance runtime loop with additional command handling for turning and connection management

--- Backend/runtime.py
+++ Backend/runtime.py
@@ -1,22 +1,57 @@
-import multiprocessing as mp
-import time
+import multiprocessing as mp, time, pycozmo
 from flask import Flask, request
 
 def runtime_loop(shared_data):
     """Handles connection to cozmo and
     processes commands from shared memory."""
+    print("[Runtime] Connecting to Cozmo...")
+    try:
+        cli = pycozmo.Client()
+        cli.start()
+        cli.connect()
+        cli.wait_for_robot(5)
+        print("[Runtime] Connected to Cozmo.")
+    except Exception as e:
+        print("[Runtime] ERROR:",e)
     print("[Runtime] Runtime loop active.")
     while True:
-        # Check shared dictionary
-        if shared_data.get("command") == "spin":
-            print("[Runtime] SPINNING!")
-            shared_data["command"] = "idle"
-            
-        elif shared_data.get("command") == "move":
-            print("[Runtime] MOVING!")
-            shared_data["command"] = "idle"
+        try:
+            # Check shared dictionary
+            if shared_data.get("command") == "turnl":
+                print("[Runtime] TURNING LEFT!")
+                shared_data["command"] = "idle"
+                cli.drive_wheels(-50,50,0.1)
 
-        time.sleep(0.01)
+            elif shared_data.get("command") == "fwd":
+                print("[Runtime] FORWARD!")
+                shared_data["command"] = "idle"
+                cli.drive_wheels(50,50,0.1)
+            
+            elif shared_data.get("command") == "turnr":
+                print("[Runtime] TURNING RIGHT!")
+                shared_data["command"] = "idle"
+                cli.drive_wheels(50,-50,0.1)
+
+            elif shared_data.get("command") == "connect":
+                print("[Runtime] CONNECTING TO COZMO!")
+                shared_data["command"] = "idle"
+                cli = pycozmo.Client()
+                cli.start()
+                cli.connect()
+                cli.wait_for_robot(5)
+                print("[Runtime] Connected to Cozmo.")
+                
+            elif shared_data.get("command") == "disconnect":
+                print("[Runtime] DISCONNECTING FROM COZMO!")
+                shared_data["command"] = "idle"
+                cli.disconnect()
+                print("[Runtime] Disconnected from Cozmo.")
+
+            time.sleep(0.01)
+        except Exception as e:
+            print("[Runtime] Error in runtime loop:",e)
+            print("continuing...")
+            
 
 app = Flask(__name__)
 shared_data = None 
