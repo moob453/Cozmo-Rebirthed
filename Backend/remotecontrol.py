125ed8e4e14fce5e446547b0454071b1887e68d7
admin.moob453 <admin@moob453.com>
2026-03-11 11:33:37 +1030

    second stage

--- Backend/remotecontrol.py
+++ Backend/remotecontrol.py
@@ -1,6 +1,7 @@
 # Test code to check runtime functionality
 import time, keyboard, os
 import requests
+state = 1
 
 def send_command(cmd):
     try:
@@ -13,29 +14,46 @@ def all_keys_released():
     return not bool(keyboard._pressed_events)
 
 def test_runtime_loop():
+    global state
     while True:
         # DriveBase
         if keyboard.is_pressed('w'):
+            state = 1
             print("FORWARD")
             send_command('fwd')
         if keyboard.is_pressed('a'):
+            state = 1
             print("TURN LEFT")
             send_command('turnl')
         if keyboard.is_pressed('d'):
+            state = 1
             print("TURN RIGHT")
             send_command('turnr')
         if keyboard.is_pressed('s'):
+            state = 1
             print("BACKWARD")
             send_command('back')
         # Lift
         if keyboard.is_pressed('up'):
+            state = 1
             print("LIFT UP")
             send_command('liftup')
         if keyboard.is_pressed('down'):
+            state = 1
             print("LIFT DOWN")
             send_command('liftdown')
+        # Head
+        if keyboard.is_pressed('y'):
+            state = 1
+            print("HEAD UP")
+            send_command('headup')
+        if keyboard.is_pressed('h'):
+            state = 1
+            print("HEAD DOWN")
+            send_command('headdown')
         #Stop all actions
-        if all_keys_released():
+        if all_keys_released() and state == 1:
+            state = 0
             print("STOP")
             send_command('stop')
 
